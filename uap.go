9b0597ffc0c288abec82bc8e6b9a5b7cf91caef6
Özgür Yildirim <ozguryenihayat@gmail.com>
2023-01-08 13:09:34 +0100

    Add admission threshold

--- uap.go
+++ uap.go
@@ -1,26 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-const numberExams = 3
+const (
+	numberExams = 3
+	threshold   = 60
+)
 
 func main() {
 	meanScore := getMeanScore()
-	showResult(meanScore)
+	decision := getDecision(meanScore)
+	showResult(decision)
 }
 
-func showResult(score float64) {
-	fmt.Println(score)
-	fmt.Println("Congratulations, you are accepted!")
+func showResult(decision bool) {
+	if decision {
+		fmt.Println("Congratulations, you are accepted!")
+	} else {
+		fmt.Println("We regret to inform you that we will not be able to offer you admission.")
+	}
 }
 
 func getMeanScore() (meanScore float64) {
 	var score float64
 	var total float64
 	for i := 0; i < numberExams; i++ {
-		fmt.Scanln(&score)
+		//fmt.Printf("Please enter the score of exam no. %d:\n", i+1)
+		_, err := fmt.Scanln(&score)
+		if err != nil {
+			log.Fatal("Enter only a number")
+		}
 		total += score
 	}
 	meanScore = total / numberExams
+	return
+}
+
+func getDecision(meanScore float64) (decision bool) {
+	fmt.Println(meanScore)
+	decision = meanScore >= threshold
 	return
 }
