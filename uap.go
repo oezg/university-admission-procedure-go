ccaac2decd2cb543afe6c289bbd027e9c5e2a803
Özgür Yildirim <ozguryenihayat@gmail.com>
2023-01-14 23:22:44 +0100

    Sort departments and write to files

--- uap.go
+++ uap.go
@@ -11,7 +11,9 @@ import (
 )
 
 const (
-	filename = "applicants.txt"
+	filename         = "applicants.txt"
+	numberPriorities = 3
+	numberExams      = 4
 )
 
 var (
@@ -22,70 +24,16 @@ var (
 )
 
 type Applicant struct {
-	firstName  string
-	lastName   string
-	gpa        float64
-	priorities [3]string
+	firstName, lastName string
+	scores              map[string]float64
+	priorities          [numberPriorities]string
 }
 
 func main() {
 	getMaxAdmitted()
 	getApplicants()
-	sortDepartment()
-	sortApplicants(applicants)
 	fillDepartments()
-	showDepartments()
-}
-
-func showDepartments() {
-	for _, departmentName := range departmentNames {
-		fmt.Println(departmentName)
-		enrollment := departments[departmentName]
-		sortApplicants(enrollment)
-		for _, student := range enrollment {
-			fmt.Printf("%v %v %.2f\n", student.firstName, student.lastName, student.gpa)
-		}
-		fmt.Println()
-	}
-}
-
-func fillDepartments() {
-	for i := 0; i < 3; i++ {
-		admitPriorityN(i)
-	}
-}
-
-func admitPriorityN(priorityRank int) {
-	k := 0
-	for _, applicant := range applicants {
-		department := applicant.priorities[priorityRank]
-		if len(departments[department]) < maxAdmitted {
-			departments[department] = append(departments[department], applicant)
-		} else {
-			applicants[k] = applicant
-			k++
-		}
-	}
-	applicants = applicants[:k]
-}
-
-func sortApplicants(applicants []Applicant) {
-	sort.Slice(applicants, func(i, j int) bool {
-		if applicants[i].gpa != applicants[j].gpa {
-			return applicants[i].gpa > applicants[j].gpa
-		}
-		if applicants[i].firstName != applicants[j].firstName {
-			return applicants[i].firstName < applicants[j].firstName
-		}
-		return applicants[i].lastName < applicants[j].lastName
-	})
-}
-
-func sortDepartment() {
-	sort.Strings(departmentNames)
-	for _, departmentName := range departmentNames {
-		departments[departmentName] = []Applicant{}
-	}
+	saveDepartments()
 }
 
 func getMaxAdmitted() {
@@ -109,17 +57,98 @@ func getApplicants() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		data := strings.SplitN(line, " ", 6)
-		pointAverage, err := strconv.ParseFloat(data[2], 64)
+		data := strings.Fields(line)
+		examScores := make([]float64, numberExams)
+		for i := 0; i < numberExams; i++ {
+			examScores[i], err = strconv.ParseFloat(data[i+2], 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		applicant := new(Applicant)
+		applicant.scores = make(map[string]float64, numberExams)
+		applicant.firstName = data[0]
+		applicant.lastName = data[1]
+		applicant.scores["Physics"] = examScores[0]
+		applicant.scores["Chemistry"] = examScores[1]
+		applicant.scores["Mathematics"] = examScores[2]
+		applicant.scores["Engineering"] = examScores[3]
+		applicant.priorities = [numberPriorities]string{data[6], data[7], data[8]}
+		applicants = append(applicants, *applicant)
+	}
+}
+
+func fillDepartments() {
+	for _, departmentName := range departmentNames {
+		departments[departmentName] = []Applicant{}
+	}
+	for priority := 0; priority < numberPriorities; priority++ {
+		for department := range departments {
+			sortApplicants(applicants, department)
+			admitDepartmentPriorityN(department, priority)
+		}
+	}
+}
+
+func sortApplicants(applicants []Applicant, department string) {
+	sort.Slice(applicants, func(i, j int) bool {
+		if getScore(applicants[i], department) != getScore(applicants[j], department) {
+			return getScore(applicants[i], department) > getScore(applicants[j], department)
+		}
+		if applicants[i].firstName != applicants[j].firstName {
+			return applicants[i].firstName < applicants[j].firstName
+		}
+		return applicants[i].lastName < applicants[j].lastName
+	})
+}
+
+func getScore(applicant Applicant, department string) float64 {
+	switch department {
+	case "Biotech":
+		return (applicant.scores["Physics"] + applicant.scores["Chemistry"]) / 2.0
+	case "Chemistry":
+		return applicant.scores[department]
+	case "Engineering":
+		return (applicant.scores[department] + applicant.scores["Mathematics"]) / 2.0
+	case "Mathematics":
+		return applicant.scores[department]
+	case "Physics":
+		return (applicant.scores[department] + applicant.scores["Mathematics"]) / 2.0
+	default:
+		return 0
+	}
+}
+
+func admitDepartmentPriorityN(department string, priority int) {
+	k := 0
+	for _, applicant := range applicants {
+		if department == applicant.priorities[priority] && len(departments[department]) < maxAdmitted {
+			departments[department] = append(departments[department], applicant)
+		} else {
+			applicants[k] = applicant
+			k++
+		}
+	}
+	applicants = applicants[:k]
+}
+
+func saveDepartments() {
+	for department, enrollment := range departments {
+		filename := fmt.Sprintf("%s.txt", strings.ToLower(department))
+		file, err := os.Create(filename)
 		if err != nil {
 			log.Fatal(err)
 		}
-		newApplicant := Applicant{
-			firstName:  data[0],
-			lastName:   data[1],
-			gpa:        pointAverage,
-			priorities: [3]string{data[3], data[4], data[5]},
+		defer file.Close()
+
+		sortApplicants(enrollment, department)
+
+		for _, candidate := range enrollment {
+			score := getScore(candidate, department)
+			_, err := fmt.Fprintf(file, "%s %s %.1f\n", candidate.firstName, candidate.lastName, score)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-		applicants = append(applicants, newApplicant)
 	}
 }
